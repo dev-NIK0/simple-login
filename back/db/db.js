c9eeb3dacc1bc1bea9323f9f7dbdeffec8063775
dev-NIK0 <nickitow6@gmail.com>
2025-03-03 01:19:08 -0300

    Backend login

--- back/db/db.js
+++ back/db/db.js
@@ -1,8 +1,15 @@
 import moongose from 'mongoose';
+import dotenv from 'dotenv'; 
+dotenv.config();
+
+const USERNAME = process.env.DB_USER ;
+const PASSWORD = process.env.DB_PASSWORD;
+const HOST = process.env.DB_HOST;
+const DATABASE = process.env.DATABASE;
 
 export const connectionDatabase = async () => {
     try {
-        await moongose.connect('mongodb+srv://dev-nik0:nico123@simplelogin.0wo2q.mongodb.net/')
+        await moongose.connect(`mongodb+srv://${USERNAME}:${PASSWORD}@${DATABASE}.${HOST}`)
         .then(db => console.log("Conectado a la base de datos"));
     } catch (error) {
         console.log("Error")
